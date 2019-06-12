c990b46556f56e8c6b1a136ea190632952d48390
JRGavin <793203698@qq.com>
2019-06-12 17:07:42 +0800

    提交可运行的helloWorld版本

---
+++ src/main/java/org/gavin/springboot1/controller/HelloWorld.java
@@ -0,0 +1,28 @@
+package org.gavin.springboot1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * @Author: huangjr
+ * @DateTime: 2018/2/3 15:18
+ * @Des:
+ */
+@Controller
+public class HelloWorld {
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String hellWorld(){
+        return "HelloWorld";
+    }
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String startSuccess(){
+        return "Start Success!";
+    }
+
+
+}
